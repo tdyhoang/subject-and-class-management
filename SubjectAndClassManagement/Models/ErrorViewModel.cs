3d008e67c084cd06318dcd56a0a2fd7ea05f0411
tdyhoang <tdyhoang@gmail.com>
2024-09-25 08:35:22 +0700

    Add project files.

---
+++ SubjectAndClassManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SubjectAndClassManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
