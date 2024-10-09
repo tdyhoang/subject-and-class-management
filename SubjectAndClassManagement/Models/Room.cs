0ac37f21c77ec8db97a6f8725922dca78263d7f1
duyngo0605 <duydaik03@gmail.com>
2024-10-09 14:50:20 +0700

    Create DbContext and add some constraint in models classes

--- SubjectAndClassManagement/Models/Room.cs
+++ SubjectAndClassManagement/Models/Room.cs
@@ -1,10 +1,21 @@
-﻿namespace SubjectAndClassManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SubjectAndClassManagement.Models
 {
     public class Room
     {
+        // Existing properties
+
+        [Required]
         public string RoomId { get; set; }
+
+        [Required]
         public string RoomName { get; set; }
+
+        [Required]
         public int RoomCapacity { get; set; }
+
+        [Required]
         public string BuildingName { get; set; }
     }
 }
--- SubjectAndClassManagement/Models/Teacher.cs
+++ SubjectAndClassManagement/Models/Teacher.cs
@@ -1,10 +1,24 @@
-﻿namespace SubjectAndClassManagement.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SubjectAndClassManagement.Models
 {
     public class Teacher
     {
+        // Existing properties
+
+        [Required]
         public string TeacherId { get; set; }
+
+        [Required]
         public string TeacherName { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [Phone]
         public string PhoneNumber { get; set; }
     }
+
 }
