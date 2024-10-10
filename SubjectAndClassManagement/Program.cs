144c4628abb23fc772607cb86eb27abe149c00a3
tdyhoang <tdyhoang@gmail.com>
2024-10-10 16:32:12 +0700

    add dbcontext into program

--- SubjectAndClassManagement/Program.cs
+++ SubjectAndClassManagement/Program.cs
@@ -1,7 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connection = String.Empty;
+if (builder.Environment.IsDevelopment())
+{
+    builder.Configuration.AddEnvironmentVariables().AddJsonFile("appsettings.Development.json");
+    connection = builder.Configuration.GetConnectionString("connection_string");
+}
+else
+{
+    connection = Environment.GetEnvironmentVariable("connection_string");
+}
+
+builder.Services.AddDbContext<SchoolContext>(options =>
+    options.UseSqlServer(connection));
+
 
 var app = builder.Build();
 
