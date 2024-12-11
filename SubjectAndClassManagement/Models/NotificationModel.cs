2c6fb5f7bde0abbcd793b549bc2ae5291eda911c
duyngo0605 <duydaik03@gmail.com>
2024-12-11 09:35:15 +0700

    Them quan ly notify

---
+++ SubjectAndClassManagement/Models/NotificationModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace SubjectAndClassManagement.Models
+{
+    public class NotificationModel
+    {
+        [Key]
+        [StringLength(10)]
+        [Display(Name = "Notify Id")]
+        public string notify_id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Title")]
+        public string notify_header { get; set; }
+
+        [Required]
+        [Display(Name = "Date")]
+        public DateTime notify_day { get; set; }
+
+        [Required]
+        [Display(Name = "Decription")]
+        public string notify_decription { get; set;}
+
+    }
+}
